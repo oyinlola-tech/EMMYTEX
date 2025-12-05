ca806581bdbeee9290e1daa0d11bd1dbb16aaeb5
Oluwayemi Oyinlola <you@example.com>
2025-12-05 21:43:55 +0100

    another one

--- .next/dev/server/chunks/_next-internal_server_app_api_sendmail_route_actions_daa82955.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/sendmail/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_sendmail_route_actions_daa82955.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oWPq5hkMHEeWsQoJ0T7YzMOyiwolApKepRGLzih48c0=\"\n}"
